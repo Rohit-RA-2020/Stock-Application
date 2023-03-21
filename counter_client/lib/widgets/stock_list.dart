6b37dfc42b8dc38b0d172676e6a7b5797924e057
Rohit Ranjan <ranjanrohit812@gmail.com>
2023-03-21 10:16:37 +0530

    Stock Dismiss Enabaled

--- counter_client/lib/widgets/stock_list.dart
+++ counter_client/lib/widgets/stock_list.dart
@@ -49,133 +49,178 @@ class MyStocksList extends ConsumerWidget {
             itemCount: portfolioSnapshot.data!['stocks'].length,
             itemBuilder: (context, index) {
               String docid = portfolioSnapshot.data!['stocks'][index]['name'];
-              return FutureBuilder(
-                future: FirebaseFirestore.instance
-                    .collection('stocks')
-                    .doc(docid)
-                    .get(),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Center(child: CircularProgressIndicator());
-                  }
-                  return GestureDetector(
-                    onTap: () {
-                      // Remove the stock from the portfolio
-                      // FirebaseFirestore.instance
-                      //     .collection('portfolio')
-                      //     .doc(userId)
-                    },
-                    child: SizedBox(
-                      height: 100,
-                      child: Card(
-                        child: Row(
-                          children: [
-                            SizedBox(
-                              width: 60,
-                              height: 60,
-                              child: CachedNetworkImage(
-                                imageUrl: snapshot.data!['image'],
-                                fit: BoxFit.fitWidth,
-                              ),
-                            ),
-                            const SizedBox(width: 10),
-                            Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  snapshot.data!['name'],
-                                  style: const TextStyle(
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                const SizedBox(height: 5),
-                                Text(
-                                  snapshot.data!['symbol'],
-                                  style: const TextStyle(
-                                    fontSize: 15,
-                                    color: Colors.grey,
-                                  ),
-                                ),
-                              ],
-                            ),
-                            const Spacer(),
-                            Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.end,
-                              children: [
-                                Row(
-                                  children: [
-                                    Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: Text(
-                                        '\$ ${snapshot.data!['price'].toString()}',
-                                        style: const TextStyle(
-                                          fontSize: 18,
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                      ),
-                                    ),
-                                    index % 2 == 0
-                                        ? const Padding(
-                                            padding:
-                                                EdgeInsets.only(right: 2.0),
-                                            child: Icon(
-                                              Icons
-                                                  .arrow_drop_down_circle_outlined,
-                                              color: Colors.red,
-                                            ),
-                                          )
-                                        : const Padding(
-                                            padding:
-                                                EdgeInsets.only(right: 2.0),
-                                            child: RotatedBox(
-                                              quarterTurns: -2,
-                                              child: Icon(
-                                                Icons
-                                                    .arrow_drop_down_circle_outlined,
-                                                color: Colors.green,
-                                              ),
-                                            ),
-                                          )
-                                  ],
-                                ),
-                                const SizedBox(height: 5),
-                                Row(
-                                  children: [
-                                    const Padding(
-                                      padding: EdgeInsets.all(0.0),
-                                      child: Text(
-                                        'Units bought:',
-                                        style: TextStyle(
-                                          fontSize: 14,
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                      ),
-                                    ),
-                                    Padding(
-                                      padding: const EdgeInsets.all(4.0),
-                                      child: Text(
-                                        portfolioSnapshot.data!['stocks'][index]
-                                                ['volume']
-                                            .toString(),
-                                        style: const TextStyle(
-                                          fontSize: 14,
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                      ),
-                                    )
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
+              return Dismissible(
+                key: Key(docid),
+                background: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Delete',
+                      style: GoogleFonts.robotoMono(
+                        fontSize: 14,
+                        color: Colors.redAccent,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const Icon(
+                      Icons.delete,
+                      color: Colors.redAccent,
+                    ),
+                  ],
+                ),
+                onDismissed: (direction) {
+                  // Remove the item from the data source.
+                  FirebaseFirestore.instance
+                      .collection('portfolio')
+                      .doc(userId)
+                      .update({
+                    'stocks': FieldValue.arrayRemove([
+                      {'name': docid}
+                    ])
+                  });
+                  // Then show a snackbar.
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      backgroundColor: // use scaaffold color here, not the snackbar color
+                          Colors.red,
+                      content: Text(
+                        "Stock Removed, Transaction will occur in 24 hours",
+                        style: GoogleFonts.robotoMono(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
                       ),
                     ),
                   );
                 },
+                child: FutureBuilder(
+                  future: FirebaseFirestore.instance
+                      .collection('stocks')
+                      .doc(docid)
+                      .get(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(child: CircularProgressIndicator());
+                    }
+                    return GestureDetector(
+                      onTap: () {
+                        // Remove the stock from the portfolio
+                        // FirebaseFirestore.instance
+                        //     .collection('portfolio')
+                        //     .doc(userId)
+                      },
+                      child: SizedBox(
+                        height: 100,
+                        child: Card(
+                          child: Row(
+                            children: [
+                              SizedBox(
+                                width: 60,
+                                height: 60,
+                                child: CachedNetworkImage(
+                                  imageUrl: snapshot.data!['image'],
+                                  fit: BoxFit.fitWidth,
+                                ),
+                              ),
+                              const SizedBox(width: 10),
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    snapshot.data!['name'],
+                                    style: const TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 5),
+                                  Text(
+                                    snapshot.data!['symbol'],
+                                    style: const TextStyle(
+                                      fontSize: 15,
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              const Spacer(),
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: [
+                                  Row(
+                                    children: [
+                                      Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Text(
+                                          '\$ ${snapshot.data!['price'].toString()}',
+                                          style: const TextStyle(
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                      ),
+                                      index % 2 == 0
+                                          ? const Padding(
+                                              padding:
+                                                  EdgeInsets.only(right: 2.0),
+                                              child: Icon(
+                                                Icons
+                                                    .arrow_drop_down_circle_outlined,
+                                                color: Colors.red,
+                                              ),
+                                            )
+                                          : const Padding(
+                                              padding:
+                                                  EdgeInsets.only(right: 2.0),
+                                              child: RotatedBox(
+                                                quarterTurns: -2,
+                                                child: Icon(
+                                                  Icons
+                                                      .arrow_drop_down_circle_outlined,
+                                                  color: Colors.green,
+                                                ),
+                                              ),
+                                            )
+                                    ],
+                                  ),
+                                  const SizedBox(height: 5),
+                                  Row(
+                                    children: [
+                                      const Padding(
+                                        padding: EdgeInsets.all(0.0),
+                                        child: Text(
+                                          'Units bought:',
+                                          style: TextStyle(
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.all(4.0),
+                                        child: Text(
+                                          portfolioSnapshot.data!['stocks']
+                                                  [index]['volume']
+                                              .toString(),
+                                          style: const TextStyle(
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
               );
             },
           );
